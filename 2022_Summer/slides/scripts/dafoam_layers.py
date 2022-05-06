c14ac8f6fa795aea76fefce72b565f896e95f0fb
Ping He <phe@iastate.edu>
2022-05-05 22:35:25 -0500

    update.

--- 2022_Summer/slides/scripts/dafoam_layers.py
+++ 2022_Summer/slides/scripts/dafoam_layers.py
@@ -10,16 +10,16 @@ x = XDSM(use_sfmath=False)
 
 x.add_input("OpenFOAM", "\\textrm{src/adjoint}")
 x.add_input("Cython", "\\textrm{src/pyDASolvers}")
-x.add_input("Python", "\\textrm{pyDAFoam.py, mphys\_dafoam.py}")
+x.add_input("Python", "\\textrm{pyDAFoam.py}, \\textrm{mphys\_dafoam.py}")
 
 x.add_system("OpenFOAM", FUNC, "\\textrm{OpenFOAM}")
 x.add_system("Cython", FUNC, "\\textrm{Cython}")
 x.add_system("Python", FUNC, "\\textrm{Python}")
 
 x.connect("OpenFOAM", "Cython", "\\textrm{libDAFoam*.so}")
-x.connect("Cython", "Python", "\\textrm{pyDASolver*.so}")
+x.connect("Cython", "Python", "\\textrm{pyDASolver*.so}, \\textrm{pyColoring*.so}")
 
-#x.add_process(["ImplicitEqn", "Objective"],arrow=True)
+x.add_process(["OpenFOAM", "Cython", "Python"], arrow=True)
 
 x.add_output("OpenFOAM", "\\textrm{OpenFOAM/sharedLibs/}", side="right")
 x.add_output("Cython", "\\textrm{dafoam/}", side="right")
