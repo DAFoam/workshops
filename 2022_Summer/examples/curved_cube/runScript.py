5f83fd09169b9b68d3179f3d89f0c6fc1d356f79
Ping He <phe@iastate.edu>
2022-05-06 20:43:42 -0500

    Added dafoam_lite.

---
+++ 2022_Summer/examples/curved_cube/runScript.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from mpi4py import MPI
+from dafoam import PYDAFOAM
+
+# Set the parameters for optimization
+daOptions = {
+    "solverName": "DASimpleFoam",
+    "primalMinResTol": 1.0e-8,
+    "useAD": {"mode": "fd"},
+    "objFunc": {
+        "CD": {
+            "part1": {
+                "type": "force",
+                "source": "patchToFace",
+                "patches": ["wing"],
+                "directionMode": "fixedDirection",
+                "direction": [1.0, 0.0, 0.0],
+                "scale": 1.0,
+                "addToAdjoint": True,
+            }
+        },
+    },
+}
+
+DASolver = PYDAFOAM(options=daOptions, comm=MPI.COMM_WORLD)
+DASolver()
+funcs = {}
+evalFuncs = ["CD", "CL"]
+DASolver.evalFunctions(funcs, evalFuncs)
---
+++ 2022_Summer/examples/dafoam_lite/src/adjoint/DASolver/DASimpleFoam/DASimpleFoam.C
@@ -0,0 +1,201 @@
+/*---------------------------------------------------------------------------*\
+
+    DAFoam  : Discrete Adjoint with OpenFOAM
+    Version : v3
+
+    This class is modified from OpenFOAM's source code
+    applications/solvers/incompressible/simpleFoam
+
+    OpenFOAM: The Open Source CFD Toolbox
+
+    Copyright (C): 2011-2016 OpenFOAM Foundation
+
+    OpenFOAM License:
+
+        OpenFOAM is free software: you can redistribute it and/or modify it
+        under the terms of the GNU General Public License as published by
+        the Free Software Foundation, either version 3 of the License, or
+        (at your option) any later version.
+    
+        OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+        ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+        FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+        for more details.
+    
+        You should have received a copy of the GNU General Public License
+        along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "DASimpleFoam.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+
+defineTypeNameAndDebug(DASimpleFoam, 0);
+addToRunTimeSelectionTable(DASolver, DASimpleFoam, dictionary);
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+DASimpleFoam::DASimpleFoam(
+    char* argsAll,
+    PyObject* pyOptions)
+    : DASolver(argsAll, pyOptions),
+      simplePtr_(nullptr),
+      pPtr_(nullptr),
+      UPtr_(nullptr),
+      phiPtr_(nullptr),
+      alphaPorosityPtr_(nullptr),
+      laminarTransportPtr_(nullptr),
+      turbulencePtr_(nullptr),
+      daTurbulenceModelPtr_(nullptr),
+      daFvSourcePtr_(nullptr),
+      fvSourcePtr_(nullptr),
+      MRFPtr_(nullptr)
+{
+}
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+void DASimpleFoam::initSolver()
+{
+    /*
+    Description:
+        Initialize variables for DASolver
+    */
+
+    Info << "Initializing fields for DASimpleFoam" << endl;
+    Time& runTime = runTimePtr_();
+    fvMesh& mesh = meshPtr_();
+#include "createSimpleControlPython.H"
+#include "createFieldsSimple.H"
+#include "createAdjointIncompressible.H"
+    // initialize checkMesh
+    daCheckMeshPtr_.reset(new DACheckMesh(daOptionPtr_(), runTime, mesh));
+
+    daLinearEqnPtr_.reset(new DALinearEqn(mesh, daOptionPtr_()));
+
+    this->setDAObjFuncList();
+
+    // initialize fvSource and compute the source term
+    const dictionary& allOptions = daOptionPtr_->getAllOptions();
+    if (allOptions.subDict("fvSource").toc().size() != 0)
+    {
+        hasFvSource_ = 1;
+        Info << "Initializing DASource" << endl;
+        word sourceName = allOptions.subDict("fvSource").toc()[0];
+        word fvSourceType = allOptions.subDict("fvSource").subDict(sourceName).getWord("type");
+        daFvSourcePtr_.reset(DAFvSource::New(
+            fvSourceType, mesh, daOptionPtr_(), daModelPtr_(), daIndexPtr_()));
+    }
+}
+
+label DASimpleFoam::solvePrimal(
+    const Vec xvVec,
+    Vec wVec)
+{
+    /*
+    Description:
+        Call the primal solver to get converged state variables
+
+    Input:
+        xvVec: a vector that contains all volume mesh coordinates
+
+    Output:
+        wVec: state variable vector
+    */
+
+#include "createRefsSimple.H"
+#include "createFvOptions.H"
+
+    // change the run status
+    daOptionPtr_->setOption<word>("runStatus", "solvePrimal");
+
+    // call correctNut, this is equivalent to turbulence->validate();
+    daTurbulenceModelPtr_->updateIntermediateVariables();
+
+    Info << "\nStarting time loop\n"
+         << endl;
+
+    // deform the mesh based on the xvVec
+    this->pointVec2OFMesh(xvVec);
+
+    // check mesh quality
+    label meshOK = this->checkMesh();
+
+    if (!meshOK)
+    {
+        this->writeFailedMesh();
+        return 1;
+    }
+
+    // if the forwardModeAD is active, we need to set the seed here
+#include "setForwardADSeeds.H"
+
+    word divUScheme = "div(phi,U)";
+    if (daOptionPtr_->getSubDictOption<label>("runLowOrderPrimal4PC", "active"))
+    {
+        if (daOptionPtr_->getSubDictOption<label>("runLowOrderPrimal4PC", "isPC"))
+        {
+            Info << "Using low order div scheme for primal solution .... " << endl;
+            divUScheme = "div(pc)";
+        }
+    }
+
+    primalMinRes_ = 1e10;
+    label printInterval = daOptionPtr_->getOption<label>("printInterval");
+    label printToScreen = 0;
+    while (this->loop(runTime)) // using simple.loop() will have seg fault in parallel
+    {
+
+        printToScreen = this->isPrintTime(runTime, printInterval);
+
+        if (printToScreen)
+        {
+            Info << "Time = " << runTime.timeName() << nl << endl;
+        }
+
+        p.storePrevIter();
+
+        // --- Pressure-velocity SIMPLE corrector
+        {
+#include "UEqnSimple.H"
+#include "pEqnSimple.H"
+        }
+
+        laminarTransport.correct();
+        daTurbulenceModelPtr_->correct();
+
+        if (printToScreen)
+        {
+            daTurbulenceModelPtr_->printYPlus();
+
+            this->printAllObjFuncs();
+
+            Info << "ExecutionTime = " << runTime.elapsedCpuTime() << " s"
+                 << "  ClockTime = " << runTime.elapsedClockTime() << " s"
+                 << nl << endl;
+        }
+
+        runTime.write();
+    }
+
+    this->writeAssociatedFields();
+
+    this->calcPrimalResidualStatistics("print");
+
+    // primal converged, assign the OpenFoam fields to the state vec wVec
+    this->ofField2StateVec(wVec);
+
+    // write the mesh to files
+    mesh.write();
+
+    Info << "End\n"
+         << endl;
+
+    return this->checkResidualTol();
+}
+
+} // End namespace Foam
+
+// ************************************************************************* //
